822e672a0387007b11f1cd6e5db6f8d9c4274d3f
jalopino <paolosaturnino7@gmail.com>
2025-09-01 07:10:36 +0800

    changes

--- app/keygo/page.tsx
+++ app/keygo/page.tsx
@@ -6,7 +6,7 @@ export default function KeygoPage() {
   return (
     <main className="min-h-fit bg-gray-50">
       <div className="container mx-auto px-4 py-4 lg:py-8">
-        <header className="text-center mb-4 lg:mb-8">
+        <header className="text-start mb-4 lg:mb-8">
           <h1 className="text-2xl lg:text-4xl font-bold text-gray-900 mb-2 font-rethink">
             KEYGO Maker
           </h1>
